f1f306dbf12cdb02e0a2e968062858db7da047e4
Hamza Gorcevic <88393813+HamzaGorcevic@users.noreply.github.com>
2024-12-18 01:48:57 +0100

    Update urls.py

--- pyvezi_backend/game/urls.py
+++ pyvezi_backend/game/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("computer_move/", views.computer_move, name="computer_move"),
-    path("alive/",views.alive,name= "alive")
+    path("alive",views.alive,name= "alive")
     # path("comp_vs_comp/", views.comp_vs_comp, name="comp_vs_comp")
 ]
